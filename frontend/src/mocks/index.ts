e03f4ccd594e48df2849961e73f705540bc597fb
haru <hinagoro5656@gmail.com>
2025-12-14 19:49:39 +0900

    fix(frontend): モックのserver用を削除

--- frontend/src/mocks/index.ts
+++ frontend/src/mocks/index.ts
@@ -1,7 +1,6 @@
 async function initMocks() {
   if (typeof window === "undefined") {
-    const { server } = await import("./server");
-    server.listen();
+    return;
   } else {
     const { worker } = await import("./browser");
     worker.start();
