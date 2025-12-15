834a190a1bcc97a9083e0a7b7caadcea59cfd834
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-12-15 15:21:13 +0900

    fix(frontend):bun run format:fixを実行

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -9,11 +9,9 @@ function App() {
   return (
     <main className="w-full min-h-svh flex items-center bg-secondary text-primary">
       <div className="w-full flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-extrabold" >Advent Sphere</h1>
+        <h1 className="text-4xl font-extrabold">Advent Sphere</h1>
         <h3 className="mt-2">アドベントスフィア</h3>
-        <Button
-          className="mt-8"
-        >
+        <Button className="mt-8">
           <Link to="/new">新しい部屋を作る</Link>
         </Button>
       </div>
