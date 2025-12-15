17769451910f6cd8ed1396338996b92529815f73
haru <hinagoro5656@gmail.com>
2025-12-15 23:01:18 +0900

    fix: インスタンスのメモ化

--- frontend/src/features/room/room.tsx
+++ frontend/src/features/room/room.tsx
@@ -1,9 +1,10 @@
 import { SplatMesh } from "@sparkjsdev/spark";
+import { useMemo } from "react";
 
 export function Room() {
   const splatURL =
     "https://pub-9aac194b4eba425aa8cc73a3d9afd14b.r2.dev/static/room.spz";
-  const splat = new SplatMesh({ url: splatURL });
+  const splat = useMemo(() => new SplatMesh({ url: splatURL }), []);
 
   return (
     <primitive
